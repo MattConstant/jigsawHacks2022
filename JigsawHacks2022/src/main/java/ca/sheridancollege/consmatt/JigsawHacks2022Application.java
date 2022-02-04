3174f832755bd6afb9f6046fa549bd982b1591ac
Matthieu Constant <matthieu.constant7792@gmail.com>
2022-02-04 16:23:04 -0500

    First Commit
    Creation of the main branch

---
+++ JigsawHacks2022/src/main/java/ca/sheridancollege/consmatt/JigsawHacks2022Application.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.consmatt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JigsawHacks2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JigsawHacks2022Application.class, args);
+	}
+
+}
---
+++ JigsawHacks2022/src/test/java/ca/sheridancollege/consmatt/JigsawHacks2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.consmatt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JigsawHacks2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
